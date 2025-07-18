a177ef42749db9700ffcc25a239e7e9f78865124
Prem <katyain1prem@gmail.com>
2025-07-18 17:12:50 +0530

    updated add.py

--- app.py
+++ app.py
@@ -67,4 +67,6 @@ def index():
                            result=result, amount=amount, message=message)
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Get PORT from environment, fallback to 5000
+    app.run(host="0.0.0.0", port=port)
+
